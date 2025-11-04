cbe6a3788db52287879d2a8b17c8abe00a0beb96
larrisim <larrisim@gmail.com>
2025-11-04 08:00:31 -0500

    Image fix attempts reduce image size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,29 @@
 "use client";
-import Image from "next/image";
+//import Image from "next/image";
 export default function Hero() {
   return (
     <section className="w-full flex flex-col items-center text-center">
       {/* Responsive full-width container with intrinsic height */}
       <div className="relative w-full" style={{ aspectRatio: "16/9" }}>
-        <Image
+        {/* <Image
           src="/images/profile.png"
           alt="Larry Chang"
           fill
           style={{ objectFit: "contain", objectPosition: "center" }}
           priority
-        />
+        /> */}
+
+        <img
+          src="/images/profile.png"
+          alt="Larry Chang"
+          style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "contain",
+          objectPosition: "center",
+          display: "block"
+          }}
+/>
       </div>
       <br></br><br></br><br></br><br></br>
 
